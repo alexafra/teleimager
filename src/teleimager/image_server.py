9046e9104c2583e3e67619ccd4aae03a1b5ea024
alexafra <alexanderfrazis@gmail.com>
2026-07-29 16:15:17 +0000

    Fix logging_mp initialization in image server

--- src/teleimager/image_server.py
+++ src/teleimager/image_server.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging_mp
-logging_mp.basicConfig(level=logging_mp.INFO)
-logger_mp = logging_mp.getLogger(__name__)
+logging_mp.basic_config(level=logging_mp.INFO)
+logger_mp = logging_mp.get_logger(__name__)
 import os
 import argparse
 import glob
